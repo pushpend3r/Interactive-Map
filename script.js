7fd012608ff6a5234530873e71869bc88af6aecc
Pushpender Singh <piyushthakur5678@gmail.com>
2020-07-30 15:50:58 +0530

    Done!

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-// let mapButtons = document.querySelectorAll(".btn");
-let mapImagesMap = new Map();
+let MapOfImages = new Map();
 
 document.querySelectorAll(".map > img").forEach((e) => {
-  mapImagesMap.set(e.id, e);
+  MapOfImages.set(e.id, e);
 });
+
+let previousButtonClick = null;
 
 document.querySelectorAll(".btn").forEach((mapButton) => {
   mapButton.addEventListener("mouseover", () => {
     let class_name = mapButton.className.split(" ")[1];
     class_name = class_name.slice(class_name.indexOf("-") + 1);
-    mapImagesMap.get(class_name).classList.replace("img-hidden", "img-visible");
-    mapImagesMap.get("original").classList.replace("img-visible", "img-hidden");
+    MapOfImages.get(class_name).classList.replace("img-hidden", "img-visible");
+    MapOfImages.get("original").classList.replace("img-visible", "img-hidden");
   });
 
   mapButton.addEventListener("mouseout", () => {
     let class_name = mapButton.className.split(" ")[1];
     class_name = class_name.slice(class_name.indexOf("-") + 1);
-    mapImagesMap.get(class_name).classList.replace("img-visible", "img-hidden");
-    mapImagesMap.get("original").classList.replace("img-hidden", "img-visible");
+    MapOfImages.get(class_name).classList.replace("img-visible", "img-hidden");
+    MapOfImages.get("original").classList.replace("img-hidden", "img-visible");
+  });
+
+  mapButton.addEventListener("click", () => {
+    let class_name = mapButton.className.split(" ")[1];
+    class_name = class_name.slice(class_name.indexOf("-") + 1);
+    MapOfImages.get(class_name).classList.replace("img-hidden", "img-visible");
+
+    if (previousButtonClick !== null) {
+      previousButtonClick.classList.replace("img-visible", "img-hidden");
+    }
+
+    previousButtonClick = MapOfImages.get(class_name);
+
+    MapOfImages.get("original").classList.replace("img-visible", "img-hidden");
   });
 });
